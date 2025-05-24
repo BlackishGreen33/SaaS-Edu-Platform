cbfb83eb5f6a488d39b6752cdbe59cb92245bb8d
BlackishGreen33 <s5460703@gmail.com>
2025-05-24 11:11:36 +0800

    ✨ feat(components)：Add Navbar

---
+++ src/common/components/navbar/NavItems.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+import { cn } from '@/common/utils';
+
+const navItems = [
+  { label: '主页', href: '/' },
+  { label: '伙伴', href: '/companions' },
+  { label: '我的旅程', href: 'my-journey' },
+];
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  return (
+    <nav className="flex items-center gap-4">
+      {navItems.map(({ label, href }) => (
+        <Link
+          href={href}
+          key={label}
+          className={cn(pathname === href && 'text-primary font-semibold')}
+        >
+          {label}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default NavItems;
