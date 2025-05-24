35ef0846fdc812e4aeebdf558056cde5f7cb616e
BlackishGreen33 <s5460703@gmail.com>
2025-05-24 10:21:38 +0800

    ✨ feat(components)：Add shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { NextPage } from 'next';
 
+import { Button } from '@/common/components/ui/button';
+
 const HomePage: NextPage = () => (
   <div>
     <h1>Home Page</h1>
+    <Button>123</Button>
   </div>
 );
 
